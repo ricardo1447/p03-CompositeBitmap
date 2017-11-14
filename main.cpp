228e3b9790e4ea68eb76e40f08f6a362969a5466
ricardo1447 <rflores42@mail.csuchico.edu>
2017-11-13 22:33:00 -0800

    Added prototypes

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 //prototypes
 
+vector < string > name(vector < string >);//asks user for file name
+
+bool validBmp(string);//checks if input is a valid file
+
+void startingSize(string);//gets starting size for first file
+
+vector < vector < vector <Pixel> > > rgbValues(vector < string >&);//used for the rgb values of each pixel
+
+Bitmap avgImage(vector < vector < vector < Pixel > > >&);//averages each image pixel by pixel
+
 int main()
 {
 
